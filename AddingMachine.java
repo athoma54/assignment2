9fa653631a07112da983827e34139fa3d050aa03
athoma54 <56282916+athoma54@users.noreply.github.com>
2019-10-08 15:21:12 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,22 +1,51 @@
+/*Alisha Thomas
+ * 1212688371
+ * Dr. Calliss
+ * CSE360 Wednesday 70642
+ * Assignment 2
+ * Description: The code for this assignment is creating an Adding Machine which is able
+ * 				to concatenate the values that's being passed into the parameters of the add
+ * 				and the subtract methods. 
+ */
+
 package cse360assign2;
-public class AddingMachine {
-    private int total;
-    public AddingMachine (){
-    total = 0;  // not needed - included for clarity
-    }
-    public int getTotal (){
-     return 0;
-    }
-    public void add (int value){
-  
-    }
-    public void subtract (int value){
-  
-    }
-    public String toString (){
-      return "";
-    }
-    public void clear(){
-  
-    }
-  }
+public class AddingMachine 
+{
+	private int total;
+	private String history = "0";
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	public int getTotal () 
+	{
+		return total;
+	}
+	public void add (int value) 
+	{
+		total = total + value;
+		history = history + " " + "+" + " " + value;
+	}
+	public void subtract (int value) 
+	{
+		total = total - value;
+		history = history.concat(" " + "-"+ " "+ value);
+	}
+	public String toString () 
+	{
+		return history;
+	}
+	public void clear() 
+	{
+
+	}
+	public static void main(String [] args )
+	{
+		AddingMachine calculate = new AddingMachine();
+		calculate.add(4);
+		calculate.subtract(2);
+		calculate.add(5);
+		calculate.subtract(9);
+		System.out.println(calculate.toString());
+	}
+}
