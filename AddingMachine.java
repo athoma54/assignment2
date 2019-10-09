fea2c39a41b206eead74d7f10b8ac529232d274c
athoma54 <56282916+athoma54@users.noreply.github.com>
2019-10-08 17:23:54 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -17,26 +17,47 @@ public class AddingMachine
 	{
 		total = 0;  // not needed - included for clarity
 	}
-	public int getTotal () 
+	
+	//this method returns the total of a method and depends on 
+	//if the method is adding or subtracting the value from
+	//the total
+		public int getTotal () 
 	{
 		return total;
 	}
+	
+	//this methods adds the value to the total by setting it to 
+	//total and then it concatenates it by storing the value of 
+	//history into history with appropriate signs and the value
+	//that is being passed into the parameters of the method
 	public void add (int value) 
 	{
 		total = total + value;
 		history = history + " " + "+" + " " + value;
 	}
+	
+	//this method subtracts the value to the total by setting it
+	//to total and then it concatenates it by storing the value 
+	//of history into history with the right signs and the value
+	//that is being passed into the parameters of the method
 	public void subtract (int value) 
 	{
 		total = total - value;
-		history = history.concat(" " + "-"+ " "+ value);
+		history = history.concat(" " + "-" + " " + value);
 	}
+	
+	//this method returns history which is concatenating either 
+	//the subtract or add method
 	public String toString () 
 	{
 		return history;
 	}
+	
+	//sets total and history to 0 after its gone through the functions
+	//and have returned both the total and the history
 	public void clear() 
 	{
-
+		total  = 0;
+		history = "0";
 	}
 }
